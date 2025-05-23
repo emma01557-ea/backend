bb345863255c0af32e1f1c80cdada338a26bad1e
Emmanuel Alvarez <emma01557@gmail.com>
2025-05-22 23:45:29 -0300

    commit 23May25

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const cors = require('cors');
+//app.use(cors());
+app.use(cors({
+  origin: '*', // Cambia a tu frontend URL en producción para más seguridad
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 // Conexión a MongoDB
