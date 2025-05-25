1c48306c4430f64912834ea854e0594863991480
Emmanuel Alvarez <emma01557@gmail.com>
2025-05-24 21:05:01 -0300

    commit 24May25

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.DEV
   ? '/api' // proxy en desarrollo (localhost)
  // : 'https://backend-5j48usd29-emmanuels-projects-d9e6d035.vercel.app/api'; // URL real en producción
-    :'https://backend-6xj0.onrender.com';
+    :'https://backend-6xj0.onrender.com/api';
 export default API_BASE_URL;
